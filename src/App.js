a5731428b4fac9894644be0f5a8b3920bc6cf0b3
Marie-Carleen Frei <lene@7freiheiten.de>
2022-03-04 15:01:11 +0100

    Add item component and functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import listItems from "./data";
 import ListItem from "./components/ListItem";
 import { nanoid } from "nanoid";
+import { useState } from "react";
+import AddItem from "./components/AddItem";
 
 function App() {
-  function addTodo(title) {
-    const newTodo = {
+  const [shoppingList, setShoppingList] = useState(listItems);
+
+  function addItem(name) {
+    const newItem = {
       _id: nanoid(),
-      name: { en: title, de: "" },
+      name: { en: name, de: "" },
     };
+    setShoppingList([...shoppingList, newItem]);
   }
+
   return (
-    <ul>
-      {listItems.map((item) => (
-        <ListItem key={item._id} name={item.name.en} />
-      ))}
-    </ul>
+    <>
+      <h1>My shopping list</h1>
+
+      <ul role="list">
+        {shoppingList.map((item) => (
+          <ListItem key={item._id} name={item.name.en} />
+        ))}
+      </ul>
+      <AddItem onAddItem={addItem} />
+    </>
   );
 }
 
