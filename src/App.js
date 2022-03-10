6556f6744cd90a25a1cb471e89c1d97ddf818eb0
Marie-Carleen Frei <lene@7freiheiten.de>
2022-03-10 07:53:14 +0100

    Implement fetch

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import styled from "styled-components";
 import { useState } from "react";
 import AddItem from "./components/AddItem";
 import ShoppingList from "./components/ShoppingList";
+import SearchItem from "./components/SearchItem";
+import { useEffect } from "react";
 
 function App() {
   const [shoppingList, setShoppingList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchedItems, setSearchedItems] = useState([]);
+
+  useEffect(() => {
+    if (searchTerm) loadShoppingItems();
+    async function loadShoppingItems() {
+      try {
+        const response = await fetch(
+          "https://fetch-me.vercel.app/api/shopping/items"
+        );
+        const data = await response.json();
+        setSearchedItems(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, [searchTerm]);
 
   return (
     <Wrapper>
       <h1>My shopping list</h1>
       <ShoppingList shoppingList={shoppingList} onDelete={deleteItem} />
       <AddItem onAddItem={addItem} />
+      <SearchItem searchTerm={searchTerm} onSearch={setSearchTerm} />
     </Wrapper>
   );
 
