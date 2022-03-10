e8b9a090736c3e92091f27bd30ea1189c06189e7
Marie-Carleen Frei <lene@7freiheiten.de>
2022-03-10 07:24:02 +0100

    Add styles to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { nanoid } from "nanoid";
+import styled from "styled-components";
 import { useState } from "react";
 import AddItem from "./components/AddItem";
 import ShoppingList from "./components/ShoppingList";
@@ -7,11 +8,11 @@ function App() {
   const [shoppingList, setShoppingList] = useState([]);
 
   return (
-    <>
+    <Wrapper>
       <h1>My shopping list</h1>
       <ShoppingList shoppingList={shoppingList} onDelete={deleteItem} />
       <AddItem onAddItem={addItem} />
-    </>
+    </Wrapper>
   );
 
   function addItem(name) {
@@ -29,3 +30,8 @@ function App() {
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 500px;
+`;
