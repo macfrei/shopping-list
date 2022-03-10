c50ad9262e596c7c21f2f08421d587d428b93dd8
Marie-Carleen Frei <lene@7freiheiten.de>
2022-03-10 07:41:40 +0100

    Add minimal styling to form

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,24 +1,32 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 export default function AddItem({ onAddItem }) {
   const [item, setItem] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    onAddItem(item);
-    setItem("");
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <label htmlFor="new-item">What do you want to buy?</label>
       <input
         type="text"
         id="new-item"
         onChange={(event) => setItem(event.target.value)}
         value={item}
+        required
       />
       <button>Add</button>
-    </form>
+    </Form>
   );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onAddItem(item);
+    setItem("");
+  }
 }
+
+const Form = styled.form`
+  display: grid;
+  gap: 12px;
+  padding: 20px;
+`;
